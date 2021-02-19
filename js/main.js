b2e362f06bc501bb8f1c7511cf3fb839e95ab8a6
Jaroslav <vidrimers2@gmail.com>
2021-02-19 05:42:22 +0300

    add slick-dlider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(function () {
-   $('.top__slider').slick();
-   
+    $('.top__slider').slick({
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        fade: true,
+    });
+
 });
